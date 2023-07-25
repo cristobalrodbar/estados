ba24b6418d5bf5e4653266b2e4c589a30944b722
Cristóbal Rodríguez <cristobalrodbar@gmail.com>
2023-07-25 17:03:53 -0600

    cubit state management

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,4 +1,7 @@
+import 'package:estados/bloc/usuario/usuario_cubit.dart';
+import 'package:estados/models/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Pagina1Page extends StatelessWidget {
   const Pagina1Page({super.key});
@@ -8,6 +11,11 @@ class Pagina1Page extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('pagina1'),
+        actions: [
+          IconButton(
+              onPressed: () => context.read<UsuarioCubit>().borrarUsuario(),
+              icon: const Icon(Icons.exit_to_app))
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -15,15 +23,43 @@ class Pagina1Page extends StatelessWidget {
         },
         child: const Icon(Icons.accessibility_new),
       ),
-      body: const InformacionUsuario(),
+      body: BodyScaffold(),
+    );
+  }
+}
+
+class BodyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<UsuarioCubit, UsuarioState>(
+      builder: (_, state) {
+        switch (state.runtimeType) {
+          case UsuarioInitial:
+            return const Center(child: Text('No hay información del usuario'));
+          case UsuarioActivo:
+            return InformacionUsuario((state as UsuarioActivo).usuario);
+          default:
+            return const Center(child: Text('Estado no reconocido'));
+        }
+        //print(state);
+        /* if (state is UsuarioInitial) {
+          return const Center(
+            child: Text('No hay información del usuario'),
+          );
+        } else if (state is UsuarioActivo) {
+          return InformacionUsuario(state.usuario);
+        } else {
+          return const Center(child: Text('Estado no reconocido'));
+        } */
+      },
     );
   }
 }
 
 class InformacionUsuario extends StatelessWidget {
-  const InformacionUsuario({
-    super.key,
-  });
+  final Usuario usuario;
+
+  const InformacionUsuario(this.usuario);
 
   @override
   Widget build(BuildContext context) {
@@ -33,22 +69,27 @@ class InformacionUsuario extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text(
+        children: [
+          const Text(
             'General',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
-          Divider(),
-          ListTile(title: Text('Nombre: ')),
-          ListTile(title: Text('Edad: ')),
-          Text(
+          const Divider(),
+          ListTile(title: Text('Nombre: ${usuario.nombre}')),
+          ListTile(title: Text('Edad: ${usuario.edad}')),
+          const Text(
             'Profesiones',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
-          Divider(),
+          const Divider(),
+          ...usuario.profesiones
+              .map((profesion) => ListTile(
+                    title: Text(profesion),
+                  ))
+              .toList()
+          /* ListTile(title: Text('Profesión 1')),
           ListTile(title: Text('Profesión 1')),
-          ListTile(title: Text('Profesión 1')),
-          ListTile(title: Text('Profesión 1')),
+          ListTile(title: Text('Profesión 1')), */
         ],
       ),
     );
--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -1,10 +1,14 @@
+import 'package:estados/bloc/usuario/usuario_cubit.dart';
+import 'package:estados/models/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Pagina2Page extends StatelessWidget {
   const Pagina2Page({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final usuarioCubit = context.read<UsuarioCubit>();
     return Scaffold(
         appBar: AppBar(
           title: const Text('pagina2'),
@@ -14,7 +18,13 @@ class Pagina2Page extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               MaterialButton(
-                onPressed: () {},
+                onPressed: () {
+                  final newUser = Usuario(
+                      nombre: 'Cristóbal',
+                      edad: 27,
+                      profesiones: ['Gamer', 'Programador']);
+                  usuarioCubit.seleccionarUsuario(newUser);
+                },
                 color: Colors.blue,
                 child: const Text(
                   'Establecer Usuario',
@@ -22,7 +32,9 @@ class Pagina2Page extends StatelessWidget {
                 ),
               ),
               MaterialButton(
-                onPressed: () {},
+                onPressed: () {
+                  usuarioCubit.cambiarEdad(28);
+                },
                 color: Colors.blue,
                 child: const Text(
                   'Cambiar Edad',
@@ -30,7 +42,9 @@ class Pagina2Page extends StatelessWidget {
                 ),
               ),
               MaterialButton(
-                onPressed: () {},
+                onPressed: () {
+                  usuarioCubit.agregarProfesion();
+                },
                 color: Colors.blue,
                 child: const Text(
                   'Añadir Profesión',
